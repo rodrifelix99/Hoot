8d8bb0a4fde24a140f6395929f7fa3cb309ba63c
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-07-24 07:56:01 +0100

    route user after login based on profile

--- lib/pages/login/controllers/login_controller.dart
+++ lib/pages/login/controllers/login_controller.dart
@@ -2,12 +2,16 @@ import 'package:get/get.dart';
 
 import 'package:hoot/services/auth_service.dart';
 import 'package:hoot/services/error_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../../util/routes/app_routes.dart';
 
 class LoginController extends GetxController {
   /// Initiates Google sign in using [AuthService].
   Future<void> signInWithGoogle() async {
     try {
-      await AuthService.signInWithGoogle();
+      final credential = await AuthService.signInWithGoogle();
+      await _handleUserCredential(credential);
     } catch (e, s) {
       await ErrorService.reportError(e, message: 'signInFailed'.tr, stack: s);
     }
@@ -16,9 +20,22 @@ class LoginController extends GetxController {
   /// Initiates Apple sign in using [AuthService].
   Future<void> signInWithApple() async {
     try {
-      await AuthService.signInWithApple();
+      final credential = await AuthService.signInWithApple();
+      await _handleUserCredential(credential);
     } catch (e, s) {
       await ErrorService.reportError(e, message: 'signInFailed'.tr, stack: s);
+    }
+  }
+
+  Future<void> _handleUserCredential(UserCredential credential) async {
+    final uid = credential.user?.uid;
+    if (uid == null) return;
+    final doc =
+        await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    if (doc.exists) {
+      Get.offAllNamed(AppRoutes.home);
+    } else {
+      Get.offAllNamed(AppRoutes.welcome);
     }
   }
 }
