61460ab42f946a0470ccf0db71feeab0b420649e
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-07-25 11:14:41 +0100

    feat(login): show welcome description

---
+++ test/login_view_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+import 'package:hoot/pages/login/controllers/login_controller.dart';
+import 'package:hoot/pages/login/views/login_view.dart';
+import 'package:hoot/theme/theme.dart';
+import 'package:hoot/util/translations/app_translations.dart';
+import 'package:hoot/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hoot/models/user.dart';
+
+class FakeAuthService extends GetxService implements AuthService {
+  @override
+  U? get currentUser => null;
+
+  @override
+  Future<U?> fetchUser() async => null;
+
+  @override
+  Future<void> signOut() async {}
+
+  @override
+  Future<UserCredential> signInWithGoogle() async => throw UnimplementedError();
+
+  @override
+  Future<UserCredential> signInWithApple() async => throw UnimplementedError();
+}
+
+void main() {
+  testWidgets('LoginView shows welcome description', (tester) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    binding.window.physicalSizeTestValue = const Size(1080, 1920);
+    binding.window.devicePixelRatioTestValue = 1.0;
+    addTearDown(binding.window.clearPhysicalSizeTestValue);
+    addTearDown(binding.window.clearDevicePixelRatioTestValue);
+    FlutterError.onError = (details) {};
+    addTearDown(() {
+      FlutterError.onError = FlutterError.dumpErrorToConsole;
+    });
+    Get.put<AuthService>(FakeAuthService());
+    Get.put(LoginController());
+
+    await tester.pumpWidget(
+      GetMaterialApp(
+        translations: AppTranslations(),
+        locale: const Locale('en'),
+        theme: AppTheme.lightTheme,
+        home: const LoginView(),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('welcomeDescription'.tr), findsOneWidget);
+    addTearDown(Get.reset);
+  });
+}
