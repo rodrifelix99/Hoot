c3bb9d2ad67f70757ea00918d2c77a88b6269227
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-07-25 13:49:41 +0100

    fix(home): initialize nested controllers

--- lib/pages/home/bindings/home_binding.dart
+++ lib/pages/home/bindings/home_binding.dart
@@ -1,9 +1,19 @@
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
+import '../../feed/controllers/feed_controller.dart';
+import '../../explore/controllers/explore_controller.dart';
+import '../../create_post/controllers/create_post_controller.dart';
+import '../../notifications/controllers/notifications_controller.dart';
+import '../../profile/controllers/profile_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => HomeController());
+    Get.lazyPut(() => FeedController());
+    Get.lazyPut(() => ExploreController());
+    Get.lazyPut(() => CreatePostController());
+    Get.lazyPut(() => NotificationsController());
+    Get.lazyPut(() => ProfileController());
   }
 }
