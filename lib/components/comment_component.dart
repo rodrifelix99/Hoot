7f69d9420b1a73fcf0aa6c67710a6f8b1dce18ce
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-07-27 19:13:54 +0100

    feat: parse mentions in posts and comments

--- lib/components/comment_component.dart
+++ lib/components/comment_component.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hoot/components/avatar_component.dart';
 import 'package:hoot/components/name_component.dart';
 import 'package:hoot/models/comment.dart';
+import '../util/mention_utils.dart';
 
 class CommentComponent extends StatelessWidget {
   final Comment comment;
@@ -18,7 +19,12 @@ class CommentComponent extends StatelessWidget {
       title: comment.user != null
           ? NameComponent(user: comment.user!, size: 14)
           : const SizedBox.shrink(),
-      subtitle: Text(comment.text),
+      subtitle: RichText(
+        text: TextSpan(
+          style: Theme.of(context).textTheme.bodyMedium,
+          children: parseMentions(comment.text),
+        ),
+      ),
     );
   }
 }
