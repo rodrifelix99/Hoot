ae6943b1d0963de912abadbec30e13596b3db4ad
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-07-30 21:05:20 +0100

    Store blurhash for uploaded post images

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -3,10 +3,19 @@ import 'dart:typed_data';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image/image.dart' as img;
+import 'package:blurhash/blurhash.dart';
 
 /// Interface for uploading post media to Firebase Storage.
+class UploadedPostImage {
+  final String url;
+  final String blurHash;
+
+  UploadedPostImage({required this.url, required this.blurHash});
+}
+
 abstract class BaseStorageService {
-  Future<List<String>> uploadPostImages(String postId, List<File> files);
+  Future<List<UploadedPostImage>> uploadPostImages(
+      String postId, List<File> files);
 }
 
 /// Default implementation uploading to the `posts` folder.
@@ -17,13 +26,15 @@ class StorageService implements BaseStorageService {
       : _storage = storage ?? FirebaseStorage.instance;
 
   @override
-  Future<List<String>> uploadPostImages(String postId, List<File> files) async {
-    final urls = <String>[];
+  Future<List<UploadedPostImage>> uploadPostImages(
+      String postId, List<File> files) async {
+    final results = <UploadedPostImage>[];
     for (var i = 0; i < files.length; i++) {
       final file = files[i];
       final ref = _storage.ref().child('posts').child(postId).child('$i.jpg');
 
       Uint8List data = await file.readAsBytes();
+      String hash = '';
       final decoded = img.decodeImage(data);
       if (decoded != null) {
         img.Image processed = decoded;
@@ -32,12 +43,13 @@ class StorageService implements BaseStorageService {
           processed = img.copyResize(processed, width: 1080);
         }
         data = Uint8List.fromList(img.encodeJpg(processed, quality: 85));
+        hash = await BlurHash.encode(data, 4, 3);
       }
 
       await ref.putData(data, SettableMetadata(contentType: 'image/jpeg'));
       final url = await ref.getDownloadURL();
-      urls.add(url);
+      results.add(UploadedPostImage(url: url, blurHash: hash));
     }
-    return urls;
+    return results;
   }
 }
