ecfadacaec0818f62920b0c795983e7d93aebb85
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-07-31 09:08:31 +0100

    Add subscription manager service and update controllers

--- lib/dependency_injector.dart
+++ lib/dependency_injector.dart
@@ -5,6 +5,7 @@ import 'services/theme_service.dart';
 import 'services/post_service.dart';
 import 'services/subscription_service.dart';
 import 'services/feed_request_service.dart';
+import 'services/subscription_manager.dart';
 import 'services/quick_actions_service.dart';
 
 /// Registers global dependencies for the application.
@@ -19,6 +20,7 @@ class DependencyInjector {
     Get.put<BasePostService>(PostService(authService: auth), permanent: true);
     Get.put(SubscriptionService(), permanent: true);
     Get.put(FeedRequestService(), permanent: true);
+    Get.put(SubscriptionManager(), permanent: true);
     Get.put(QuickActionsService(), permanent: true);
     final theme = Get.put(ThemeService(), permanent: true);
     await theme.loadThemeMode();
