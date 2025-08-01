1aaad1c8c1baee535f659e50cc66b54f07e75604
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-01 10:30:34 +0100

    Remove unused logger and color utils

--- lib/util/color_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Returns either [Colors.white] or [Colors.black] depending on the luminance
-/// of the given [background] color. Colors with a luminance greater than 0.5
-/// are considered light and will use [Colors.black] as the foreground color,
-/// otherwise [Colors.white] is used.
-Color foregroundForBackground(Color background) {
-  return background.computeLuminance() > 0.5 ? Colors.black : Colors.white;
-}
