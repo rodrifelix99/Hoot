783c9b7f4c452ae56b44832def9c843d1a38fcf0
Felix <rodrifelix99@gmail.com>
2025-08-02 13:59:25 +0100

    Clean up tree

--- functions/src/config.ts
+++
@@ -1,8 +0,0 @@
-import { setGlobalOptions } from "firebase-functions";
-import { initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-
-setGlobalOptions({ maxInstances: 10 });
-initializeApp();
-
-export const db = getFirestore();
