a114c5d06c9549cd0454f0d760f9d391df2d0574
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-03 21:31:33 +0100

    feat: track uninvited user count

--- lib/services/stats_service.dart
+++ lib/services/stats_service.dart
@@ -4,11 +4,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Stats {
   final int totalUsers;
   final int activeUsers;
+  final int uninvitedUsers;
   final int reportsCount;
 
   Stats({
     required this.totalUsers,
     required this.activeUsers,
+    required this.uninvitedUsers,
     required this.reportsCount,
   });
 }
@@ -39,10 +41,22 @@ class StatsService implements BaseStatsService {
           .get(),
     ]);
 
+    final usersSnapshot = results[0] as QuerySnapshot<Map<String, dynamic>>;
+    final uninvitedUsers = usersSnapshot.docs.where((doc) {
+      final data = doc.data();
+      final code = data['invitationCode'];
+      final invitedBy = data['invitedBy'];
+      return code == null ||
+          (code is String && code.isEmpty) ||
+          invitedBy == null ||
+          (invitedBy is String && invitedBy.isEmpty);
+    }).length;
+
     return Stats(
-      totalUsers: results[0].size,
+      totalUsers: usersSnapshot.size,
       activeUsers: results[1].size,
       reportsCount: results[2].size,
+      uninvitedUsers: uninvitedUsers,
     );
   }
 }
