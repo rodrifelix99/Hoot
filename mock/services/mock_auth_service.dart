57ef02d09bded2e1925633ad3b718bb2939fa448
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-03 16:34:43 +0100

    test: cover ExploreController adult content filtering

--- mock/services/mock_auth_service.dart
+++ mock/services/mock_auth_service.dart
@@ -7,29 +7,31 @@ import '../data/mock_user.dart';
 
 /// Mock implementation of [AuthService] providing in-memory user data.
 class MockAuthService extends real.AuthService {
-  MockAuthService()
-      : super(auth: MockFirebaseAuth(), firestore: FakeFirebaseFirestore());
+  final U user;
+  MockAuthService({DateTime? createdAt})
+      : user = createMockUser(createdAt: createdAt),
+        super(auth: MockFirebaseAuth(), firestore: FakeFirebaseFirestore());
 
   @override
   Future<U?> fetchUser() async {
-    currentUserRx.value = mockUser;
+    currentUserRx.value = user;
     return currentUserRx.value;
   }
 
   @override
   Future<U?> fetchUserById(String uid) async {
-    return uid == mockUser.uid ? mockUser : null;
+    return uid == user.uid ? user : null;
   }
 
   @override
   Future<U?> fetchUserByUsername(String username) async {
-    return username == mockUser.username ? mockUser : null;
+    return username == user.username ? user : null;
   }
 
   @override
   Future<List<U>> searchUsers(String query, {int limit = 5}) async {
-    if (mockUser.username != null && mockUser.username!.startsWith(query)) {
-      return [mockUser];
+    if (user.username != null && user.username!.startsWith(query)) {
+      return [user];
     }
     return [];
   }
