f89b5ec33ef6e8f6e96918fe43c194fed186f7c3
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-05 23:26:10 +0100

    Store notification permission denial flag

--- lib/pages/home/controllers/home_controller.dart
+++ lib/pages/home/controllers/home_controller.dart
@@ -1,10 +1,11 @@
 import 'package:get/get.dart';
-import 'package:hoot/services/quick_actions_service.dart';
-import 'package:screen_corner_radius/screen_corner_radius.dart';
+import 'package:hoot/pages/notifications/controllers/notifications_controller.dart';
 import 'package:hoot/services/auth_service.dart';
 import 'package:hoot/services/onesignal_service.dart';
+import 'package:hoot/services/quick_actions_service.dart';
 import 'package:hoot/util/routes/app_routes.dart';
-import 'package:hoot/pages/notifications/controllers/notifications_controller.dart';
+import 'package:screen_corner_radius/screen_corner_radius.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeController extends GetxController {
   final selectedIndex = 0.obs;
@@ -42,7 +43,10 @@ class HomeController extends GetxController {
 
     final oneSignal = Get.find<OneSignalService>();
     await oneSignal.login(user.uid);
-    if (await oneSignal.canRequestPermission()) {
+    final prefs = await SharedPreferences.getInstance();
+    const prefKey = 'notificationPermissionDenied';
+    final permissionDenied = prefs.getBool(prefKey) ?? false;
+    if (!permissionDenied && await oneSignal.canRequestPermission()) {
       Get.offAllNamed(AppRoutes.notificationsPermission);
       return;
     }
