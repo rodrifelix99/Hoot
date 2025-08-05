a835a9919a741c2c488804c5a931c0b69ecffbf5
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-06 00:17:25 +0100

    Store notification permission denial flag (#373)

--- lib/pages/notifications_permission/controllers/notification_permission_controller.dart
+++ lib/pages/notifications_permission/controllers/notification_permission_controller.dart
@@ -1,12 +1,19 @@
 import 'package:get/get.dart';
 import 'package:hoot/services/onesignal_service.dart';
 import 'package:hoot/util/routes/app_routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotificationPermissionController extends GetxController {
   final OneSignalService _oneSignalService = Get.find<OneSignalService>();
+  static const _prefKeyNotificationPermissionDenied =
+      'notificationPermissionDenied';
 
   Future<void> requestPermission() async {
-    await _oneSignalService.requestPermission();
+    final granted = await _oneSignalService.requestPermission();
+    if (!granted) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setBool(_prefKeyNotificationPermissionDenied, true);
+    }
     Get.offAllNamed(AppRoutes.home);
   }
 }
