597006f61df73a61c9822e43d916c62472afa29e
Felix <rodrifelix99@gmail.com>
2025-08-05 07:48:49 +0100

    Delete tests and mock

--- mock/services/mock_notification_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-import 'package:hoot/services/notification_service.dart';
-
-/// Mock implementation of [BaseNotificationService] backed by
-/// [FakeFirebaseFirestore].
-class MockNotificationService extends NotificationService {
-  MockNotificationService({FakeFirebaseFirestore? firestore})
-      : super(firestore: firestore ?? FakeFirebaseFirestore());
-}
--- mock/services/mock_subscription_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-import 'package:hoot/services/subscription_service.dart';
-
-/// Mock implementation of [SubscriptionService] using [FakeFirebaseFirestore].
-class MockSubscriptionService extends SubscriptionService {
-  MockSubscriptionService({FakeFirebaseFirestore? firestore})
-      : super(firestore: firestore ?? FakeFirebaseFirestore());
-}
