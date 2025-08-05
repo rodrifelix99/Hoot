c5731e5732ce144441ec0c1acd81a48b27e27310
Felix <rodrifelix99@gmail.com>
2025-08-05 11:43:38 +0100

    Refactor: Add background image to invite friends page
    This commit adds a background image to the invite friends page.
    - Wraps the `Obx` widget in a `Stack` to allow layering of widgets.
    - Adds an `Image.asset` widget to display `assets/images/top_bar.png` at the bottom of the screen.

--- lib/pages/invite_friends/views/invite_friends_view.dart
+++ lib/pages/invite_friends/views/invite_friends_view.dart
@@ -17,93 +17,112 @@ class InviteFriendsView extends GetView<InviteFriendsController> {
       appBar: AppBarComponent(
         title: 'inviteFriends'.tr,
       ),
-      body: Obx(
-        () => Center(
-          child: TicketWidget(
-            width: 350,
-            height: 500,
-            isCornerRounded: true,
-            padding: EdgeInsets.all(16),
-            color: Theme.of(context).colorScheme.primaryContainer,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/images/top_bar.png',
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+          ),
+          Obx(
+            () => Center(
+              child: TicketWidget(
+                width: 350,
+                height: 500,
+                isCornerRounded: true,
+                padding: EdgeInsets.all(16),
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      padding: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(32),
-                        border: Border.all(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 1,
-                        ),
-                      ),
-                      child: Text(
-                        'yourInviteCode'.tr,
-                        style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                    Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(32),
+                            border: Border.all(
                               color: Theme.of(context).colorScheme.primary,
+                              width: 1,
                             ),
+                          ),
+                          child: Text(
+                            'yourInviteCode'.tr,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall
+                                ?.copyWith(
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                          ),
+                        ),
+                        const Spacer(),
+                        IconButton(
+                          icon: const Icon(SolarIconsOutline.copy),
+                          onPressed: () {
+                            Clipboard.setData(ClipboardData(
+                                text: controller.inviteCode.value));
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(SolarIconsOutline.share),
+                          onPressed: () {
+                            Share.share(controller.inviteCode.value);
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    Center(
+                      child: Text(
+                        controller.inviteCode.value,
+                        key: const Key('inviteCode'),
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.displayMedium,
                       ),
                     ),
-                    const Spacer(),
-                    IconButton(
-                      icon: const Icon(SolarIconsOutline.copy),
-                      onPressed: () {
-                        Clipboard.setData(
-                            ClipboardData(text: controller.inviteCode.value));
-                      },
+                    const SizedBox(
+                      height: 8,
                     ),
-                    IconButton(
-                      icon: const Icon(SolarIconsOutline.share),
-                      onPressed: () {
-                        Share.share(controller.inviteCode.value);
-                      },
+                    Center(
+                      child: Text(
+                        'invitesLeftThisMonth'.trParams({
+                          'count': controller.remainingInvites.value.toString(),
+                        }),
+                        textAlign: TextAlign.center,
+                        key: const Key('invitesLeft'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    Text(
+                      'Quick tips:',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Hoot is invite-only. That means the only way in is through someone who\'s already inside. Like you.\nYour invite code is unique. Share it with friends, mutuals, or that one guy who keeps asking what app this is. When they use your code during sign-up, they get instant access to the full Hoot experience 🪶',
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 9,
                     ),
                   ],
                 ),
-                const SizedBox(
-                  height: 64,
-                ),
-                Center(
-                  child: Text(
-                    controller.inviteCode.value,
-                    key: const Key('inviteCode'),
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.displayMedium,
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Center(
-                  child: Text(
-                    'invitesLeftThisMonth'.trParams({
-                      'count': controller.remainingInvites.value.toString(),
-                    }),
-                    textAlign: TextAlign.center,
-                    key: const Key('invitesLeft'),
-                  ),
-                ),
-                const SizedBox(
-                  height: 64,
-                ),
-                Text(
-                  'Quick tips:',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  'Hoot is invite-only. That means the only way in is through someone who\'s already inside. Like you.\nYour invite code is unique. Share it with friends, mutuals, or that one guy who keeps asking what app this is. When they use your code during sign-up, they get instant access to the full Hoot experience 🪶',
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 9,
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
