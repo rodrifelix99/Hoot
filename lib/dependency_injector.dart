20ac66ebef790dc986319cef32d510d623a75546
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-06 09:27:59 +0100

    Prompt for rating using in-app review (#375)

--- lib/dependency_injector.dart
+++ lib/dependency_injector.dart
@@ -11,6 +11,7 @@ import 'package:hoot/services/quick_actions_service.dart';
 import 'package:hoot/services/onesignal_service.dart';
 import 'package:hoot/services/language_service.dart';
 import 'package:hoot/services/news_service.dart';
+import 'package:hoot/services/app_review_service.dart';
 
 /// Registers global dependencies for the application.
 class DependencyInjector {
@@ -27,6 +28,7 @@ class DependencyInjector {
     Get.put(SubscriptionManager(), permanent: true);
     Get.put(QuickActionsService(), permanent: true);
     Get.put(OneSignalService(), permanent: true);
+    Get.put(AppReviewService(), permanent: true);
     final theme = Get.put(ThemeService(), permanent: true);
     await theme.loadThemeSettings();
     final language = Get.put(LanguageService(), permanent: true);
