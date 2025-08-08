46e43c6ccb90c78a57c7edcb05947db3d69d1a34
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-08 08:53:15 +0100

    Add widget test for base64 images

--- lib/pages/photo_view/views/photo_view.dart
+++ lib/pages/photo_view/views/photo_view.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hoot/pages/photo_view/controllers/photo_view_controller.dart';
@@ -11,8 +13,27 @@ class PhotoZoomView extends GetView<PhotoZoomViewController> {
     final imageUrl = controller.imageUrl;
     if (imageUrl.startsWith('http')) {
       return NetworkImage(imageUrl);
-    } else {
-      return AssetImage(imageUrl);
+    }
+    if (imageUrl.startsWith('data:')) {
+      final base64Data = imageUrl.split(',').last;
+      return MemoryImage(base64Decode(base64Data));
+    }
+    if (_isBase64(imageUrl)) {
+      return MemoryImage(base64Decode(imageUrl));
+    }
+    return AssetImage(imageUrl);
+  }
+
+  bool _isBase64(String str) {
+    final regex = RegExp(r'^[A-Za-z0-9+/]+={0,2}$');
+    if (str.length % 4 != 0 || !regex.hasMatch(str)) {
+      return false;
+    }
+    try {
+      base64Decode(str);
+      return true;
+    } catch (_) {
+      return false;
     }
   }
 
