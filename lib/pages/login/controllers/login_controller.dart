95b4856c55462a509c4f00881010061cfb925cc4
Felix <51212863+rodrifelix99@users.noreply.github.com>
2025-08-09 16:22:56 +0100

    feat: add analytics for auth flow

--- lib/pages/login/controllers/login_controller.dart
+++ lib/pages/login/controllers/login_controller.dart
@@ -2,27 +2,63 @@ import 'package:get/get.dart';
 
 import 'package:hoot/services/auth_service.dart';
 import 'package:hoot/services/error_service.dart';
+import 'package:hoot/services/analytics_service.dart';
 import 'package:hoot/util/routes/app_routes.dart';
 
 class LoginController extends GetxController {
   final _auth = Get.find<AuthService>();
+  AnalyticsService? get _analytics => Get.isRegistered<AnalyticsService>()
+      ? Get.find<AnalyticsService>()
+      : null;
 
   /// Initiates Google sign in using [AuthService].
   Future<void> signInWithGoogle() async {
+    if (_analytics != null) {
+      await _analytics!.logEvent('sign_in_button_pressed',
+          parameters: {'provider': 'google'});
+    }
     try {
-      await _auth.signInWithGoogle();
+      final result = await _auth.signInWithGoogle();
+      if (_analytics != null) {
+        await _analytics!.logEvent('sign_in_success', parameters: {
+          'provider': 'google',
+          if (result.user != null) 'userId': result.user!.uid,
+        });
+      }
       Get.offAllNamed(AppRoutes.home);
     } catch (e, s) {
+      if (_analytics != null) {
+        await _analytics!.logEvent('sign_in_failure', parameters: {
+          'provider': 'google',
+          'error': e.toString(),
+        });
+      }
       await ErrorService.reportError(e, message: 'signInFailed'.tr, stack: s);
     }
   }
 
   /// Initiates Apple sign in using [AuthService].
   Future<void> signInWithApple() async {
+    if (_analytics != null) {
+      await _analytics!.logEvent('sign_in_button_pressed',
+          parameters: {'provider': 'apple'});
+    }
     try {
-      await _auth.signInWithApple();
+      final result = await _auth.signInWithApple();
+      if (_analytics != null) {
+        await _analytics!.logEvent('sign_in_success', parameters: {
+          'provider': 'apple',
+          if (result.user != null) 'userId': result.user!.uid,
+        });
+      }
       Get.offAllNamed(AppRoutes.home);
     } catch (e, s) {
+      if (_analytics != null) {
+        await _analytics!.logEvent('sign_in_failure', parameters: {
+          'provider': 'apple',
+          'error': e.toString(),
+        });
+      }
       await ErrorService.reportError(e, message: 'signInFailed'.tr, stack: s);
     }
   }
